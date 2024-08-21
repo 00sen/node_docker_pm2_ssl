40665e9b9ad779bed5d98015f122df19b35a7078
sen <inarrituzanetti@outlook.com>
2024-08-21 08:16:03 -0600

    almost ready nginx with env

--- node-app/app.js
+++ node-app/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send(`c:       t(._.t)        uwu        7u7`);
 });
 
 app.listen(port, () => {
