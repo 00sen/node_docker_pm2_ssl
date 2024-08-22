73999191241347bc68c079a40b0e0fc441abf633
sen <inarrituzanetti@outlook.com>
2024-08-21 21:58:19 -0600

    ya funciona deploy de node y nginx con config correcta

--- node-app/app.js
+++ node-app/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send(`c:       t(._.t)        uwu        7u7`);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:3000`);
 });
